7f67ac57062f9a637322b58547b5cdba0655e3e5
Belma Gutlic <belma.dzi@gmail.com>
2019-08-24 23:55:58 +0200

    Create new migration with split network

---
+++ app/scripts/migrations/037.js
@@ -0,0 +1,29 @@
+const version = 37
+
+/*
+
+stores read notifications (for payment requests)
+*/
+
+const clone = require('clone')
+
+module.exports = {
+  version,
+
+  migrate: async function (originalVersionedData) {
+    const versionedData = clone(originalVersionedData)
+    versionedData.meta.version = version
+    const state = versionedData.data
+    const newState = transformState(state)
+    versionedData.data = newState
+    return versionedData
+  },
+}
+
+function transformState (state) {
+  const newState = state
+  // transform state here
+  newState.SplitNetworkController = {};
+  newState.SplitNetworkController.readNotifications = [];
+  return newState;
+}
